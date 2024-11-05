3c949f29de6e2f82f6677a81c0acb23f797a3fd0
rhiaro <amy@rhiaro.co.uk>
2024-11-05 12:13:33 +0000

    content: mailing list links

--- getinvolved.php
+++ getinvolved.php
@@ -1,7 +1,7 @@
 <? $pg="vendors"; ?>
 <? include("header.php");?>
     <h2 class="orange">Get involved</h2>
-    <p>If you'd like to have a stall, run a workshop or demonstration, or give a talk at a future VegFest event, please <a href="">sign up to be the first to get our call for vendors</a>. You can also drop us an email.</p>
+    <p>If you'd like to have a stall, run a workshop or demonstration, or give a talk at a future VegFest event, please <a href="https://subscribepage.io/vegfestvendors">sign up to be the first to get our call for vendors</a>. You can also drop us an email.</p>
     <p>There is <a href="/vendors">more information for vendors here</a>.</p>
-    <p>Anyone interested in volunteering their time to help with promotion, planning and on the day at future events, we'd love to hear from you. We have <a href="">a separate mailing list for helpers</a> so please do join it.</p>
+    <p>Anyone interested in volunteering their time to help with promotion, planning and on the day at future events, we'd love to hear from you. We have <a href="https://subscribepage.io/vegfesthelpers">a separate mailing list for helpers</a> so please do join it.</p>
 <? include("footer.php"); ?>
