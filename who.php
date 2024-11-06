bed53ce86b58ae98586b61595c59507b2a989ef8
rhiaro <amy@rhiaro.co.uk>
2024-11-06 08:51:30 +0000

    content: stallholders page placeholder

---
+++ who.php
@@ -0,0 +1,22 @@
+<? $pg="vendors"; ?>
+<? include("header.php"); ?>
+<h2 class="orange">
+Who will be at Fife VegFest?
+</h2>
+<p>
+We'll list stallholders here closer to the time.
+</p>
+<!--<ul>
+  <li><a target="_blank" href="https://thetiffin.uk">The Tiffin</a> Punjabi street food from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.fergieskitchen.co.uk">Fergie's Kitchen</a> hearty dishes from the Dunfermline food truck</li>
+  <li><a target="_blank" href="https://www.plantedbycali.co.uk/">Planted by Cali</a> seasonal organic meals from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.bodylushious.com/">Bodylushious</a> skincare from Glenrothes</li>
+  <li><a target="_blank" href="https://www.facebook.com/people/The-Kitchen-Treasury/61554324862777/">Kitchen Treasury</a> protein-packed salads from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.ecobean.co.uk/">Ecobean</a> coffee roasted in Kirkcaldy</li>
+  <li><a target="_blank" href="https://lochaberfarm.co.uk/">Lochaber Bakery</a> handmade sourdough from North East Fife</li>
+  <li><a target="_blank" href="https://www.facebook.com/people/Hughes-Bakery-Company/">Hughes Bakery</a> doughnuts as big as yer head, and more, from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.facebook.com/people/Couple-O-Mugs/100091429974761/">Couple o Mugs</a> cafe vibes from Kirkcaldy</li>
+  <li><a href="https://linktr.ee/solutionpubco">Betty Nicols</a> Kirkcaldy pub launching their new plant-based menu</li>
+  <li>Plant Based Fife local vegan community group</li>
+</ul>-->
+<? include("footer.php"); ?>
