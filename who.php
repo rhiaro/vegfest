8da9ed5c6030e2cc8ca6ada1603914a3113588cb
rhiaro <amy@rhiaro.co.uk>
2024-11-15 13:22:15 +0000

    content: description for east neuk seaweed

--- who.php
+++ who.php
@@ -9,15 +9,15 @@ We're looking forward to vegan treats from:
 <ul>
   <li><a target="_blank" href="https://thetiffin.uk">The Tiffin</a> Punjabi street food from Kirkcaldy</li>
   <li><a target="_blank" href="https://cookiedotdreams.wordpress.com">QBee Rustique</a> allergy-friendly sweet treats from Kirkcaldy</li>
-  <li><a target="_blank" href="https://blissbitesscotland.co.uk/">Bliss Bites</a> healthy snacks from Inchture</li>
   <li><a target="_blank" href="https://www.nakeybakeyco.com/">Nakey Bakey</a> allergen-free granola from Methil</li>
   <li><a target="_blank" href="https://www.rawwildandconscious.com/">Raw Wild & Conscious</a> superfood chocolate from Cardenden</li>
+  <li><a target="_blank" href="https://blissbitesscotland.co.uk/">Bliss Bites</a> healthy snacks from Inchture</li>
   <li><a target="_blank" href="https://www.facebook.com/kamaveganbakez/">Kama Vegan Bakes</a> savoury vegan bakes from Edinburgh</li>
-  <li><a target="_blank" href="https://www.facebook.com/people/Hughes-Bakery-Company/">Hughes Bakery</a> doughnuts as big as yer head, and more, from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.facebook.com/people/Hughes-Bakery-Company/">Hughes Bakery</a> doughnuts as big as yer head from Kirkcaldy</li>
   <li><a target="_blank" href="https://www.facebook.com/people/The-Kitchen-Treasury/61554324862777/">Kitchen Treasury</a> protein-packed salads from Kirkcaldy</li>
   <li><a target="_blank" href="https://lochaberfarm.co.uk/">Lochaber Bakery</a> handmade sourdough from North East Fife</li>
   <li><a target="_blank" href="https://www.ecobean.co.uk/">Ecobean</a> coffee beans roasted and ground in Kirkcaldy</li>
-  <li><a target="_blank" href="https://www.eastneukseaweed.com/">East Neuk Seaweed</a></li>
+  <li><a target="_blank" href="https://www.eastneukseaweed.com/">East Neuk Seaweed</a> wild-foraged coastal food from North East Fife</li>
   <li><a target="_blank" href="https://fido-handmade-dog-treats.sumupstore.com/">Fido's Luxury Dog Treats</a> handmade locally sourced snacks for your pooch from Kirkcaldy</li>
   <li><a target="_blank" href="https://www.bodylushious.com/">Bodylushious</a> skincare and candles from Glenrothes</li>
   <li> ... more to come!</li>
