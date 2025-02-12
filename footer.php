eb00ff47d44c725c94d4ce35e44aa0fdad57daf4
rhiaro <amy@rhiaro.co.uk>
2025-02-12 09:44:52 +0000

    fix: images work from subfolders

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <footer>
     <div class="line">
       <h2 class="pink">Contact</h2>
-      <p><a href="https://facebook.com/profile.php?id=61565917130972"><img src="img/icon_fb.png" /></a> <a href="https://instagram.com/vegfestfife"><img src="img/icon_insta.png" /></a> <a href="mailto:vegfest@kdy.scot"><img src="img/icon_email.png" /> vegfest@kdy.scot</a></p>
+      <p><a href="https://facebook.com/profile.php?id=61565917130972"><img src="/img/icon_fb.png" /></a> <a href="https://instagram.com/vegfestfife"><img src="/img/icon_insta.png" /></a> <a href="mailto:vegfest@kdy.scot"><img src="/img/icon_email.png" /> vegfest@kdy.scot</a></p>
     </div>
     <div class="line">
       <h2 class="pink"><a href="https://subscribepage.io/fifevegfest">Future events</a></h2>
