3bc5267bc213f0395afb3dd345396d2e3b88ffaa
rhiaro <amy@rhiaro.co.uk>
2025-02-12 09:45:14 +0000

    vf2: update vendors

--- who.php
+++ who.php
@@ -8,13 +8,11 @@ We're looking forward to vegan treats from:
 </p>
 <ul>
   <li><a target="_blank" href="https://thetiffin.uk">The Tiffin</a> Punjabi street food from Kirkcaldy</li>
-  <li><a target="_blank" href="https://www.plantedbycali.co.uk/">Planted by Cali</a> seasonal organic meals from Kirkcaldy</li>
   <li><a target="_blank" href="https://cookiedotdreams.wordpress.com">QBee Rustique</a> allergy-friendly sweet treats from Kirkcaldy</li>
-  <li><a target="_blank" href="https://www.nakeybakeyco.com/">Nakey Bakey</a> allergen-free granola from Methil</li>
   <li><a target="_blank" href="https://www.rawwildandconscious.com/">Raw Wild & Conscious</a> superfood chocolate from Cardenden</li>
   <li><a target="_blank" href="https://www.facebook.com/aberdourjamsandchutneys/">Aberdour Jams & Chutneys</a> delicious spreads from Aberdour</li>
+  <li><a target="_blank" href="https://www.clearfife.org/">CLEAR</a> locally grown juices, cordials and preserves from Buckhaven</li>
   <li><a target="_blank" href="https://blissbitesscotland.co.uk/">Bliss Bites</a> healthy snacks from Inchture</li>
-  <li><a target="_blank" href="https://www.marwicksvegankitchen.com/">Marwick's</a> hot and cold meals and snacks from Dundee</li>
   <li><a target="_blank" href="https://www.facebook.com/kamaveganbakez/">Kama Vegan Bakes</a> savoury vegan bakes from Edinburgh</li>
   <li><a target="_blank" href="https://www.facebook.com/people/Hughes-Bakery-Company/">Hughes Bakery</a> doughnuts as big as yer head from Kirkcaldy</li>
   <li><a target="_blank" href="https://lochaberfarm.co.uk/">Lochaber Bakery</a> handmade sourdough from North East Fife</li>
@@ -22,8 +20,10 @@ We're looking forward to vegan treats from:
   <li><a target="_blank" href="https://www.eastneukseaweed.com/">East Neuk Seaweed</a> wild-foraged coastal food from North East Fife</li>
   <li><a target="_blank" href="https://fido-handmade-dog-treats.sumupstore.com/">Fido's Luxury Dog Treats</a> handmade locally sourced snacks for your pooch from Kirkcaldy</li>
   <li><a target="_blank" href="https://www.bodylushious.com/">Bodylushious</a> skincare and candles from Glenrothes</li>
-  <li> ... more to come!</li>
-  <!-- <li><a target="_blank" href="https://www.facebook.com/people/Couple-O-Mugs/100091429974761/">Couple o Mugs</a> cafe vibes from Kirkcaldy</li> -->
 </ul>
-<p>Fife VegFest is organised in conjunction with Plant Based Fife, a local vegan community group.</p>
+<p>Fife VegFest is organised in conjunction with Plant Based Fife, a local vegan community group. We'll also have informational stalls from:</p>
+<ul>
+  <li>Local sustainability and social justice charity <a target="_blank" href="https://www.greenerkirkcaldy.org.uk/">Greener Kirkcaldy</a></li>
+  <li>Global environmental charity <a target="_blank" href="https://greenpeace.org">Greenpeace</a>.</li>
+</ul>
 <? include("footer.php"); ?>
