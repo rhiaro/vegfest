068662f1cf72bb3cf86544e938e7530fac2adb48
rhiaro <amy@rhiaro.co.uk>
2025-02-12 09:42:29 +0000

    content: update timetable for vf2

---
+++ more.php
@@ -0,0 +1,76 @@
+<? $pg="more"; ?>
+<? include("header.php"); ?>
+<h2 class="tan">
+Fife VegFest 2
+</h2>
+<p><strong>This is our second event showcasing local vegan-friendly businesses</strong>.</p>
+<p>
+We wanted to build on the success of the <a href="vegfest1">first Fife VegFest</a> in a new venue, the Old Kirk in Kirkcaldy, that allows us to have more stall holders and give even more people access to the amazing vegan food and drink that’s made locally in Fife, and slightly further afield.
+</p>
+<p>
+We are keen to promote <a href="who">local vegan businesses</a>, and give people an insight into healthy, ethical and sustainable food choices, and anyone who may be trying out Veganuary.
+</p>
+<p>
+There will be local artists running a creative corner, including badge making and block printing your very own Fife VegFest merchandise - so make sure to bring along a t-shirt, tote bag or patch you want to customise anything - and there will also be <a href="#timetable">panels</a> and a yoga workshop.
+</p>
+<p>
+There'll also be a stall with Plant Based Fife, a local group who empowers the vegan community with resources, events and advocacy for sustainable, plant-based living, and are currently calling on organisations to commit to the <a href="https://plantbasedtreaty.org/">plant-based treaty</a>. They'll be joined by local sustainability and social justice charity <a href="https://greenerkirkcaldy.org.uk">Greener Kirkcaldy</a> and global environmental campaigners <a href="https://greenpeace.org">Greenpeace</a>.
+</p>
+<p>
+There will be a raffle running during the day, with <a href="#raffle">prizes generously donated</a> by our vendors, and all proceeds go back into running future VegFests.
+</p>
+<p>
+Fife VegFest is organised and run entirely by volunteers and not for-profit!
+</p>
+<h2 class="tan" id="timetable">
+Timetable
+</h2>
+<div class="line">
+  <h3>All day</h3>
+  <p>main hall</p>
+</div>
+<ul>
+  <li>Food, drinks and treats</li>
+  <li>Creative corner: print-your-own merch (bring tshirts, bags, any other fabric), badge making, colouring, drawing, ideas wall.</li>
+  <li>The Wee Vegan Library.</li>
+  <li>Pop-up cafe, comfy seats.</li>
+</ul>
+<div class="line">
+  <h3>12:00</h3>
+  <p>the back room</p>
+</div>
+<p><strong>Panel: Vegan Mythbusting / Beyond Veganuary</strong></p>
+<p>
+Get answers to any burning questions you have about the path to plant-based eating. Biggest challenges? Favourite recipes? Where do you get your protein?
+Q&A with local vegans, everyone welcome to join in the conversation.
+</p>
+<div class="line">
+  <h3>13:00</h3>
+  <p>the back room</p>
+</div>
+<p><strong>Panel: Sustainable food choices</strong></p>
+<p>How does what we eat impact the environment? A discussion with Councillor Jan Wincott, Fife College's Sustainability Manager Hayley Williamson, and local activist Shiva Subramanian.</p>
+<div class="line">
+  <h3>14:30</h3>
+  <p>the back room</p>
+</div>
+<p><strong>Introduction to Yin Yoga</strong> with Penny</p>
+<p>Wind down with some basic relaxation postures, and set positive intentions for 2025.
+(Limited spaces, please sign up at the front desk).</p>
+<div class="line">
+  <h3>15:00</h3>
+  <p>main hall</p>
+</div>
+<p id="raffle"><strong>Raffle draw!</strong></p>
+<p>Win some amazing prizes donated by local businesses, including:</p>
+<ul>
+    <li>Tiffin for Two</li>
+    <li>Handmade chocolate from Raw Wild & Conscious</li>
+    <li>Two mains and a dessert from kt@home by Kitchen Treasury</li>
+    <li>A vintage cake from 269 Vegan</li>
+    <li>A Bodylushious gift box</li>
+    <li>A luxury breakfast hamper (coffee from Ecobean, jam from Aberdour Jams & Chutneys, bread from Lochaber Bakery, apple juice from CLEAR)</li>
+    <li>2x£5 vouchers from Couple o Mugs cafe</li>
+    <li>VegFest tote bag</li>
+</ul>
+<? include("footer.php"); ?>
