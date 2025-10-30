7338012d7301670218a753ded36e69fd9b3b2944
rhiaro <amy@rhiaro.co.uk>
2025-10-30 11:16:19 +0000

    content: page about volunteering

--- getinvolved.php
+++ getinvolved.php
@@ -3,5 +3,5 @@
     <h2 class="orange">Get involved</h2>
     <p>If you'd like to have a stall, run a workshop or demonstration, or give a talk at a future VegFest event, please <a href="https://subscribepage.io/vegfestvendors">sign up to be the first to get our call for vendors</a>. You can also drop us an email.</p>
     <p>There is <a href="/vendors">more information for vendors here</a>.</p>
-    <p>Anyone interested in volunteering their time to help with promotion, planning and on the day at future events, we'd love to hear from you. We have <a href="https://subscribepage.io/vegfesthelpers">a separate mailing list for helpers</a> so please do join it.</p>
+    <p>Anyone interested in volunteering their time to help with promotion, planning and on the day at future events, we'd love to hear from you. <a href="volunteer">Find out more here</a>.</p>
 <? include("footer.php"); ?>
---
+++ volunteer.php
@@ -0,0 +1,34 @@
+<? $pg="vendors"; ?>
+<? include("header.php");?>
+    <h2 class="orange">Volunteer with us</h2>
+    <p>Fife VegFest is run entirely by volunteers, and not-for-profit. If you'd like to help us out, we need people to...</p>
+    <p>Before an event:</p>
+    <ul>
+        <li>Put up posters in the six weeks before an event.</li>
+        <li>Share the event on social media, especially in any relevant groups.</li>
+        <li>Help planning and admin for upcoming events, eg. contacting and fielding questions from vendors; licensing paperwork; updating the website.</li>
+        <li>Help us to set up the afternoon before an event.</li>
+    </ul>
+    <p>On the day:</p>
+    <ul>
+        <li>Help us to set up in the morning.</li>
+        <li>Welcome people at the front desk.</li>
+        <li>Be 'runners', helping out vendors or visitors as needed.</li>
+        <li>Hand out flyers on the High St and nearby during quiet times.</li>
+        <li>Help out with the Creative Space (checking in with artists running workshops, overseeing the DIY craft areas).</li>
+        <li>Help with cleanup after 4pm.</li>
+    </ul>
+    <p>There is no minimum time commitment, but if you can help for 2 or more hours you've earned yourself a free lunch!</p>
+    <p><strong>If this sounds like something you're interested in, <a href="https://cloud.amy.gy/apps/forms/s/qq6MLcWTAJ36MnLpF6yxEFoA">let us know your availability using this form</a></strong>. We also encourage you to come along to the next <a href="social">social</a> and introduce yourself if we don't know you already!</p>
+    <p>Volunteers need to be 16 or older, and agree to our <a href="#coc">volunteer code of conduct</a>.</p>
+    <p>Any questions? Just <a href="mailto:vegfest@kdy.scot">drop us an email</a>.</p>
+    <br/>
+    <h3 id="coc">Volunteer Code of Conduct</h3>
+    <p>Fife VegFest operate as a safe and inclusive space.</p>
+    <p>Volunteers commit to respectful interactions with members of the public, our stallholders, and each other.</p>
+    <p>We don't tolerate any form of harassment, bullying or discriminatory behaviour. This includes racism, homophobia, transphobia, sexism, ableism, classism, misogyny, fatphobia, or any other form of bigotry.</p>
+    <p>If you become aware of anyone exhibiting this behaviour, please let one of the organisers know.</p>
+    <p>We welcome genuine curiosity and respectful questions. We avoid rude, passive aggressive or judgemental comments about peoples' dietary or life choices.</p>
+    <p>Keep in mind that one of our goals is to make vegan food interesting and accessible to a wide range of people.</p>
+    <p>We understand that topics like dietary and lifestyle choices, sustainability and environmental impact, and health are likely to come up as part of this event. Please be mindful that for some people these are emotional, sensitive, or triggering topics. Be gentle with each other.</p>
+<? include("footer.php"); ?>
