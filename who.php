878e661efdc8b0ffa7c46252098d4f85e036d86b
rhiaro <amy@rhiaro.co.uk>
2026-05-02 09:44:59 +0100

    content: vegfest 5 vendors

--- who.php
+++ who.php
@@ -4,7 +4,36 @@
 Who will you find at VegFest?
 </h2>
 <p>
-As the lineup is confirmed, we'll list the vendors you'll find at the next VegFest here.
+We love to celebrate local businesses and community organisations who offer a thoughtful range of vegan options. We prioritise traders from Fife and nearby, and keep a few slots for your faves and award-winning food from further afield in Scotland.
 </p>
+<p>
+At the fifth VegFest, we are excited to welcome:
+</p>
+<ul>
+  <li><a target="_blank" href="https://blissbitesscotland.co.uk/">Bliss Bites</a> energy balls and healthy snacks from Inchture</li>
+  <li><a target="_blank" href="https://www.bodylushious.com/">Bodylushious</a> skincare and candles from Glenrothes</li>
+  <li><a target="_blank" href="https://www.buddykombucha.co.uk/">Buddy Kombucha</a> homebrewed kombucha from Newburgh</li>
+
+  <li><a target="_blank" href="https://www.clearfife.org/">CLEAR</a> locally grown juices, cordials and preserves from Buckhaven</li>
+  <li><a target="_blank" href="https://www.consideritchocolate.com/">Considerit</a> chocolate, doughnuts and sweet treats made in Dunfermline</li>
+  <li><a target="_blank" href="https://artisanvegancheese.co.uk/">Earthy Vegan Cheese</a> handmade fermented cashew cheese from Edinburgh</li>
+  <li><a target="_blank" href="https://www.ecobean.co.uk/">Ecobean</a> ethically sourced coffee roasted in Kirkcaldy</li>
+  <li><a target="_blank" href="https://fidotreats.co.uk/shop/">Fido Luxury Dog Treats</a> homemade treats for your pooch from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.gfgf.co.uk/">Good Food Good Feelings</a> organic fermented wholefoods from Auchterhouse</li>
+  <li><a target="_blank" href="https://hottsauces.com/">Hott</a> a creative array of hot sauces from Edinburgh</li>
+  <li><a target="_blank" href="https://www.feedthesquirrel.co.uk/">Hungry Squirrel</a> nutritious nut butters from Crathes</li>
+  <li><a target="_blank" href="https://www.marwicksvegankitchen.com/">Marwick's Kitchen</a> bao and cheesecake from Tayport</li>
+  <li><a target="_blank" href="https://peachykeen.co.uk">Peachy Keen</a> solar-grown microherbs from Kirkcaldy</li>
+  <li><a target="_blank" href="https://cookiedotdreams.wordpress.com">QBee Rustique</a> allergy-friendly sweet treats from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.rawwildandconscious.com/">Raw, Wild & Conscious</a> stone-ground cacao chocolate from Cardenden</li>
+  <li><a target="_blank" href="https://www.rawwildandconscious.com/">Raw, Wild & Conscious</a> stoneground superfood chocolate from Cardenden</li>
+  <li><a target="_blank" href="https://thesanctuarygarden.co.uk/">The Sanctuary Garden</a> seedlings for your garden from Dunfermline</li>
+  <li><a target="_blank" href="https://www.simplyflo.co.uk/">Simply Flo</a> Lebanese cuisine from Auchterader</li>
+  <li><a target="_blank" href="https://www.instagram.com/skadi_bakery/">Skadi Bakery</a> sourdough bakes and Nordic patisserie from the Borders</li>
+  <li><a target="_blank" href="https://softservecartel.com/">Soft Serve Cartel</a> oat-based ice cream from East Kilbride</li>
+  <li><a target="_blank" href="https://thetiffin.uk">The Tiffin</a> Punjabi street food from Kirkcaldy</li>
+  <li><a target="_blank" href="https://www.unitbakery.co.uk/">Unit Bakery</a> luxury cakes from Kinghorn</li>
+</ul>
 <p>Fife VegFest is organised in conjunction with <a href="https://fifevegfest.org/vendors">Plant Based Fife</a>, a local vegan community group.</p>
+<? include("../footer.php"); ?>
 <? include("footer.php"); ?>
